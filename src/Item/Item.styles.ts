191ea14e973b1ff13b652286c45a51d8439239af
Micke Berg <micke.berg.dev@gmail.com>
2021-06-16 16:25:59 +0200

    Initial Commit

---
+++ src/Item/Item.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid lightgray;
+  height: 100%;
+
+  img {
+    max-height: 250px;
+    object-fit: cover;
+  }
+  div {
+    /* font-family: Arial, Helvetica, sans-serif; */
+    padding: 1rem;
+    /* height: 100%; */
+  }
+`;
